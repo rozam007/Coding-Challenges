3e2165b1a280f20ad5ea7b2d5acead5383b1d01e
rozam007 <rozam0040@gmail.com>
2025-02-16 19:04:49 +0500

    Reverse Linked List

---
+++ submissions/reverse-linked-list.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(head === null){
+        return null
+    }
+
+    if(head.next === null){
+        return head
+    }
+
+    let prevNode = null;
+    let currentNode = head;
+
+    while(currentNode !== null){
+        const nextNode = currentNode.next;
+        currentNode.next = prevNode;
+        prevNode=currentNode;
+        currentNode=nextNode;
+    }
+
+    return prevNode;
+};
