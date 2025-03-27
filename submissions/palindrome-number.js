cffc51bcbd2335fae84d755a88f12d34bb818644
rozam007 <rozam0040@gmail.com>
2025-03-27 14:15:09 +0500

    Palindrome Number

---
+++ submissions/palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x < 0) return false
+    let original = x;
+    let reversed = 0;
+    
+      while (x > 0) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    
+    return original === reversed;
+};
